f4f1d0ca11daeac47f2362261775cd03a2a3dd0a
MooseV2 <anthony@desouza.li>
2018-11-27 20:28:07 -0500

    Removed junk

--- test/client.py
+++
@@ -1,8 +0,0 @@
-import rpyc
-
-
-c = rpyc.connect_by_service("TIME")
-print( "server's time is", c.root.get_time())
-
-c = rpyc.connect_by_service("DATE")
-print( "server's time is", c.root.get_date())
